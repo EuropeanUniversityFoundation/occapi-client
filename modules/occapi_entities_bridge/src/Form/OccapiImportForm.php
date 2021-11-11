72835e758bc46a6fa32a516e6782b55d07dd9bd6
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-11-12 00:17:52 +0100

    Services injected into import form

--- modules/occapi_entities_bridge/src/Form/OccapiImportForm.php
+++ modules/occapi_entities_bridge/src/Form/OccapiImportForm.php
@@ -4,11 +4,55 @@ namespace Drupal\occapi_entities_bridge\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\occapi_client\DataFormatter;
+use Drupal\occapi_client\JsonDataFetcher;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a OCCAPI entities bridge form.
+ * Provides an OCCAPI entities import form.
  */
 class OccapiImportForm extends FormBase {
+
+  /**
+   * OCCAPI endpoint.
+   *
+   * @var string
+   */
+  protected $occapiEndpoint;
+
+  /**
+   * JSON data fetcher service.
+   *
+   * @var \Drupal\occapi_client\JsonDataFetcher
+   */
+  protected $jsonDataFetcher;
+
+  /**
+   * Data formatter service.
+   *
+   * @var \Drupal\occapi_client\DataFormatter
+   */
+  protected $dataFormatter;
+
+  /**
+   * The logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    $instance = parent::create($container);
+    $instance->jsonDataFetcher = $container->get('occapi_client.fetch');
+    $instance->dataFormatter = $container->get('occapi_client.format');
+    $instance->loggerFactory = $container->get('logger.factory');
+    $instance->logger = $instance->loggerFactory->get('occapi_entities_bridge');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -21,6 +65,9 @@ class OccapiImportForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+    dpm($this);
+    dpm($form);
 
     $form['message'] = [
       '#type' => 'textarea',
