e2bb2026f9c117b83dc1c6b689336d375964f403
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-11-13 17:49:55 +0100

    New approach to import form, similar to ewp_institutions_get

--- modules/occapi_entities_bridge/src/Routing/RouteSubscriber.php
+++ modules/occapi_entities_bridge/src/Routing/RouteSubscriber.php
@@ -14,11 +14,14 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    // Set new permission for the Programme and Course add forms.
-    if (\in_array($route, [
-       $collection->get('entity.programme.add_form'),
-       $collection->get('entity.course.add_form'),
-      ])) {
+    // Set new permission for the Programme add form.
+    if ($route = $collection->get('entity.programme.add_form')) {
+      $route->setRequirements([
+        '_permission' => 'bypass import occapi entities',
+      ]);
+    }
+    // Set new permission for the Course add form.
+    if ($route = $collection->get('entity.course.add_form')) {
       $route->setRequirements([
         '_permission' => 'bypass import occapi entities',
       ]);
