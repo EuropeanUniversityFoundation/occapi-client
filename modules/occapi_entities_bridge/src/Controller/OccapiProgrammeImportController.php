b290f46f85b37f6ad7d5bd5cfcb3096a33fddcbc
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-11-19 13:05:20 +0100

    New Controllers: import_programme and import_course_bulk

---
+++ modules/occapi_entities_bridge/src/Controller/OccapiProgrammeImportController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\occapi_entities_bridge\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for OCCAPI entities bridge routes.
+ */
+class OccapiProgrammeImportController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
